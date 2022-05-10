00761094e9275a4bc3aef62ec74de0d1582de64a
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-05-10 14:19:54 -0700

    Initial Project Check In

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -1,1 +1,20 @@
+print("Hello, my name is Alex your virtual assistant.  I will help you order a pizza!")
+print("I am going to ask you a few questions.  After typing an answer, press enter.")
+userName = input("\nEnter your name:  ")
+print("Hello, " + userName +".  Nice to meet you!")
 
+size = input("\nWhat size do you want?  Enter small, medium, or large:  ")
+flavor = input("\nEnter the flavor of pizza:  ")
+crustType = input("\nWhat type of crust do you want?:  ")
+quantity = input("\nHow many of these do you want to order?  Enter a numeric value:  ")
+quantity = int(quantity)
+method = input("\nIs this carry out or delivery?:  ")
+
+salesTax = 1.1
+pizzaCost = 14.99
+total = (pizzaCost * quantity) * salesTax
+
+print("\n------------------")
+print("Thank you, " + userName + ", for your order.")
+print("Your ", quantity, size, flavor, "pizza with", crustType, "crust cost", "${:,.2f}".format(total) + ".")
+print("\n------------------")
