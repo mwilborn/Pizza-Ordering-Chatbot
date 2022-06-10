eeda51d0c9a4995b3fab2660c5629ff298ce7f7f
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-06-10 09:02:28 -0700

    Added Data Validation

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -1,17 +1,52 @@
 print("Hello, my name is Alex your virtual assistant.  I will help you order a pizza!")
 print("I am going to ask you a few questions.  After typing an answer, press enter.")
 userName = input("\nEnter your name:  ")
+
+while (len(userName) == 0):
+    userName = input("Please enter your name:  ")
+
+
 if userName.lower() == "marvin wilborn":
     print("\nMy creator, " + userName + ".  Pleasure to serve you!")
 else:
     print("\nHello, " + userName +".  Nice to meet you!")
 
 size = input("\nWhat size do you want?  Enter small, medium, or large:  ")
+
+while size.lower() != "small" and size.lower() != "large":
+    size = input("Invalid, Invalid.  Please enter small, medium, or large:  ")
+
+while (len(size) == 0):
+    size = input("Please enter small, medium, or large:  ")
+
+
 flavor = input("\nEnter the flavor of pizza:  ")
+
+while (len(flavor) == 0):
+    flavor = input("Please enter a flavor:  ")
+
 crustType = input("\nWhat type of crust do you want?:  ")
+
+while (len(crustType) == 0):
+    crustType = input("Please enter a crust type:  ")
+
 quantity = input("\nHow many of these do you want to order?  Enter a numeric value:  ")
+
+while not quantity.isdigit():
+    quantity = input("\nValue not recognized.  Please enter a numeric value:  ")
+
+while (len(quantity) == 0):
+    quantity = input("Please enter a numeric value:  ")
+
 quantity = int(quantity)
+
 method = input("\nIs this carry out or delivery?:  ")
+
+while method.lower() != "delivery" and method.lower() != "carry out" and method.lower() != "carryout":
+    method = input("\nInvalid value.  Please enter delivery or carry out:  ")
+
+while (len(method) == 0):
+    method = input("Please enter carry out or delivery:  ")
 
 salesTax = 1.1  
 
